9d503ce07581292d20af207c4012d90d42ea455a
huaning-yang <huaning.y@gmail.com>
2021-05-15 13:05:00 +0200

    Different Style

--- repo_list.py
+++ repo_list.py
@@ -3,17 +3,15 @@ from mdutils.mdutils import MdUtils
 
 mdFile = MdUtils(file_name='README')
 
-username = input("Enter the github username:")
+username = input("Enter the github username:")                                                                                                                               
 request = requests.get('https://api.github.com/users/'+username+'/repos')
 json = request.json()
 for i in range(0,len(json)):
   repo_url = json[i]['html_url']
   repo = json[i]['name']
   releases = requests.get('https://api.github.com/repos/'+username+'/'+repo+'/releases/latest')
-  collaborators = requests.get('https://api.github.com/repos/'+username+'/'+repo+'/collaborators')
-  json3 = collaborators.json()
 
-  mdFile.new_line('___')
+  mdFile.new_line('********')
   if(releases.status_code == 200):
     json2 = releases.json()
     mdFile.new_line('Repository: ' + mdFile.new_inline_link(link=json[i]['html_url'],text=json[i]['name']) + ' ' + 'Latest Release: ' + mdFile.new_inline_link(link=json2['html_url'],text=json2['tag_name']) + '\n')
@@ -25,12 +23,8 @@ for i in range(0,len(json)):
   else: 
     mdFile.new_line(text='There is no description for this repository', bold_italics_code='i')  
 
-  if(collaborators.status_code == 200):
-    mdFile.new_line('Collaborators: ')
-    for j in range(0,len(json3)):
-      mdFile.new_inline_link(link=json3[j]['html_url'], text=json3[j]['login'])
 
-  mdFile.new_line('___')
+  mdFile.new_line('********')
 
 print("done")
 mdFile.create_md_file()
