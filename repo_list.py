ba8c5715149a876696dd80191d8abf0a1b1f7344
huaning-yang <huaning.y@gmail.com>
2021-05-16 12:57:18 +0200

    Added github actions

--- repo_list.py
+++ repo_list.py
@@ -3,25 +3,34 @@ from mdutils.mdutils import MdUtils
 
 mdFile = MdUtils(file_name='README')
 
-username = input("Enter the github username:")                                                                                                                               
+#username = input("Enter the github username:")
+username = 'iqb-berlin'                                                                                                                               
 request = requests.get('https://api.github.com/users/'+username+'/repos')
 json = request.json()
 for i in range(0,len(json)):
   repo_url = json[i]['html_url']
   repo = json[i]['name']
   releases = requests.get('https://api.github.com/repos/'+username+'/'+repo+'/releases/latest')
+  contributor = requests.get('https://api.github.com/repos/'+username+'/'+repo+'/contributors')
+  
 
   mdFile.new_line('********')
   if(releases.status_code == 200):
     json2 = releases.json()
-    mdFile.new_line('Repository: ' + mdFile.new_inline_link(link=json[i]['html_url'],text=json[i]['name']) + ' ' + 'Latest Release: ' + mdFile.new_inline_link(link=json2['html_url'],text=json2['tag_name']) + '\n')
+    mdFile.new_line('Repository: ' + mdFile.new_inline_link(link=json[i]['html_url'],text=json[i]['name']) + ' ' + 'Latest Release: ' + mdFile.new_inline_link(link=json2['html_url'],text=json2['tag_name']))
   else:
-    mdFile.new_line('Repository: ' + mdFile.new_inline_link(link=json[i]['html_url'],text=json[i]['name']) + "\n" )
+    mdFile.new_line('Repository: ' + mdFile.new_inline_link(link=json[i]['html_url'],text=json[i]['name']) )
 
   if(json[i]['description'] != None):
     mdFile.new_line('Description: ' + json[i]['description'])
   else: 
     mdFile.new_line(text='There is no description for this repository', bold_italics_code='i')  
+  
+  if(contributor.status_code == 200):
+    contributor_json = contributor.json()
+    for c in range(0,len(contributor_json)):
+        mdFile.new_line('Contributor: ' + mdFile.new_inline_link(link=contributor_json[c]['html_url'],text=contributor_json[c]['login']))
+      
 
 
   mdFile.new_line('********')
