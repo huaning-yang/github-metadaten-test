789c357d35171f438d5590ab16a32658e8dccfba
huaning-yang <huaning.y@gmail.com>
2021-05-17 14:31:22 +0200

    Include used languages

--- repo_list.py
+++ repo_list.py
@@ -4,38 +4,40 @@ from mdutils.mdutils import MdUtils
 mdFile = MdUtils(file_name='README')
 
 #username = input("Enter the github username:")
-username = 'huaning-yang'                                                                                                                               
+username = 'iqb-berlin'                                                                                                                               
 request = requests.get('https://api.github.com/users/'+username+'/repos')
-json = request.json()
-for i in range(0,len(json)):
-  repo_url = json[i]['html_url']
-  repo = json[i]['name']
+repository_json = request.json()
+for i in range(0,len(repository_json)):
+  repo_url = repository_json[i]['html_url']
+  repo = repository_json[i]['name']
   releases = requests.get('https://api.github.com/repos/'+username+'/'+repo+'/releases/latest')
   contributor = requests.get('https://api.github.com/repos/'+username+'/'+repo+'/contributors')
-  pages = requests.get('https://api.github.com/repos/'+username+'/'+repo+'/pages')
+  languages = requests.get('https://api.github.com/repos/'+username+'/'+repo+'/languages')
   
 
   mdFile.new_line('********')
   
   if(releases.status_code == 200):
-    json2 = releases.json()
-    mdFile.new_line('Repository: ' + mdFile.new_inline_link(link=json[i]['html_url'],text=json[i]['name']) + ' ' + 'Latest Release: ' + mdFile.new_inline_link(link=json2['html_url'],text=json2['tag_name']))
+    releases_json = releases.json()
+    mdFile.new_line('Repository: ' + mdFile.new_inline_link(link=repository_json[i]['html_url'],text=repository_json[i]['name']) + ' ' + 'Latest Release: ' + mdFile.new_inline_link(link=releases_json['html_url'],text=releases_json['tag_name']))
   else:
-    mdFile.new_line('Repository: ' + mdFile.new_inline_link(link=json[i]['html_url'],text=json[i]['name']) )
+    mdFile.new_line('Repository: ' + mdFile.new_inline_link(link=repository_json[i]['html_url'],text=repository_json[i]['name']) )
 
-  if(json[i]['description'] != None):
-    mdFile.new_line('Description: ' + json[i]['description'])
+  if(repository_json[i]['description'] != None):
+    mdFile.new_line('Description: ' + repository_json[i]['description'])
   else: 
     mdFile.new_line(text='There is no description for this repository', bold_italics_code='i')  
   
   if(contributor.status_code == 200):
     contributor_json = contributor.json()
     for c in range(0,len(contributor_json)):
-        mdFile.new_line('Contributor: ' + mdFile.new_inline_link(link=contributor_json[c]['html_url'],text=contributor_json[c]['login']))
+        mdFile.new_line('Contributor: ' + mdFile.new_inline_link(link=contributor_json[c]['html_url'],text=contributor_json[c]['login'])) 
 
-  if(pages.status_code == 200):
-    pages_json = pages.json()
-    mdFile.new_line(mdFile.new_inline_link(link=pages_json['html_url'],text='Github Pages'))    
+  if(languages.status_code == 200):
+    languages_json = languages.json()
+    mdFile.new_line('Languages: ')
+    for keys in languages_json.keys():
+      mdFile.write(keys + " ")
 
   mdFile.new_line('********')
 
